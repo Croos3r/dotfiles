617766d74fc8186ae8b6804b6177455bf4422967
Dorian Moy <dorian.moy@proton.me>
2025-10-12 03:11:16 +0200

    fix(polybar-crypto): accelerated scrolling and removed useless comment

--- polybar/scripts/scroll_crypto_status.sh
+++ polybar/scripts/scroll_crypto_status.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#zscroll -l 3  --delay 0.1 --scroll-padding "  " "cacacacacaca" & 2> /dev/null
-#wait
-zscroll -l 15 -u true -p " " -U 60 "`dirname $0`/crypto_status.sh" & 2> /dev/null
-wait 
+zscroll -l 15 -d 0.1 -u true -p " " -U 60 "$(dirname "$0")/crypto_status.sh" &
+2>/dev/null
+wait
