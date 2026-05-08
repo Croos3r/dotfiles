6e8a75eaaad743ecaab152ab91a276c689c975e3
Dorian Moy <dorian.moy@proton.me>
2026-05-08 15:08:30 +0200

    update

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,9 @@ if status is-interactive
     starship init fish | source
     set fish_greeting
     pyenv init - fish | source
+	fish_vi_key_bindings
 end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
